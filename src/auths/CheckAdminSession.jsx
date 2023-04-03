b07f6145de03dc8591609ae6e5cdba68375ab480
markymole <83807400+markymole@users.noreply.github.com>
2023-04-03 14:39:51 +0800

    Delete src directory

--- src/auths/CheckAdminSession.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import useAuthContext from '../context/Authentication'
-
-const CheckAdminSession = () => {
-  const { user } = useAuthContext();
-  return user ? <Navigate to="/admin"/> : <Outlet></Outlet>;
-};
-
-export default CheckAdminSession;
